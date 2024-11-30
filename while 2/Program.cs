7708d4cd6930bd17b768fdc374679652a30e4521
Jengo2007 <zuyunovmagamed@gmail.com>
2024-11-30 19:30:57 +0300

    while2

---
+++ while 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int N=int.Parse(Console.ReadLine());
+int K=int.Parse(Console.ReadLine());
+double count=0;
+
+while (N >= K)
+{
+    N-= K;
+    count++;
+}
+Console.WriteLine(N);
